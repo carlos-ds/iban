7b4a32597b335d5808aaa62a2b8f027b69e383e6
Karel De Smet <karel.de.smet@outlook.com>
2020-10-29 07:51:15 +0100

    Added an iban service and implemented the getIban() method which gets the 5 newest ibans from the db

---
+++ src/app/services/iban.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { Iban } from '../models/iban.model';
+
+const API: string = 'http://localhost:3306';
+
+@Injectable()
+export class IbanService {
+  constructor(private http: HttpClient) {}
+
+  getIban(): Observable<Iban[]> {
+    return this.http.get<Iban[]>(API);
+  }
+}
