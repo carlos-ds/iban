aeee84faace9137b84c72e8636318091bc95c3e0
Karel De Smet <karel.de.smet@outlook.com>
2020-10-31 13:24:01 +0100

    Updated variable name and imported the IbanService and ValidationResult interface

--- src/app/services/iban.service.ts
+++ src/app/services/iban.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
-import { Iban } from '../models/iban.model';
+import { Iban, ValidationResult } from '../models/iban.model';
 
 const API: string = 'http://localhost:3306';
 const numberOfPreviousIbans: number = 5;
@@ -12,11 +12,32 @@ const numberOfPreviousIbans: number = 5;
 export class IbanService {
   constructor(private http: HttpClient) {}
 
-  getIban(): Observable<Iban[]> {
+  /**
+   * Source: https://angular.io/tutorial/toh-pt6
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  get(): Observable<Iban[]> {
     return this.http.get<Iban[]>(API + '/?limit=' + numberOfPreviousIbans);
   }
 
-  createIban(): Observable<Iban[]> {
+  create(): Observable<Iban[]> {
     return this.http.get<Iban[]>(API + '/create');
+  }
+
+  validate(accountNumber: string): Observable<any> {
+    return this.http
+      .post(API + '/validate', { accountNumber })
+      .pipe(catchError(this.handleError('validateIban')));
   }
 }
