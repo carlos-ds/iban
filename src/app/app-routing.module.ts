0d975ae4d86194c445f10c6de137c29661ea11e8
Karel De Smet <karel.de.smet@outlook.com>
2020-11-01 18:22:09 +0100

    Added 404 route to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IbanComponent } from './components/iban/iban.component';
+import { AboutComponent } from './components/about/about.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: IbanComponent },
   { path: 'iban', component: IbanComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
