6736880b9d58bb4a940e989cf8ca7610bf76fd0e
Karel De Smet <karel.de.smet@outlook.com>
2020-11-01 19:03:47 +0100

    Added a server file and made some changes to package.json to get this up and running at Heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + "/dist/iban"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/<name-of-app>/index.html"));
+});
+
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`Port: ${port}\nEnvironment: ${environment}`)
+);
