1cf19c48493da5145d3ed425b02c9f96b210626d
Karel De Smet <karel.de.smet@outlook.com>
2020-11-01 18:21:24 +0100

    Finished the validation component

--- src/app/components/iban/iban-validation/iban-validation.component.ts
+++ src/app/components/iban/iban-validation/iban-validation.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { IbanService } from 'src/app/services/iban.service';
 import { ValidationResult } from '../../../models/iban.model';
 
@@ -9,35 +8,20 @@ import { ValidationResult } from '../../../models/iban.model';
   styleUrls: ['./iban-validation.component.scss'],
 })
 export class IbanValidationComponent implements OnInit {
-  showValidationResult: boolean;
   accountNumberToValidate: string;
   validationResult: ValidationResult;
 
   constructor(private ibanService: IbanService) {
     this.accountNumberToValidate = '';
-    this.showValidationResult = true;
     this.validationResult = {} as ValidationResult;
   }
 
   handleChange(event: any) {
-    const accountNumber = event.target.value;
-
-    if (this.accountNumberToValidate.substr(0, 2) === 'BE') {
-      this.validationResult.startsWithBelgianPrefix = true;
-    }
-    if (this.accountNumberToValidate.length === 16) {
-      this.validationResult.has16Characters = true;
-    }
-  }
-
-  onFocus() {
-    this.showValidationResult = true;
-  }
-
-  onFocusOut() {
-    if (this.accountNumberToValidate.length === 0) {
-      this.showValidationResult = false;
-    }
+    this.ibanService
+      .validate({ accountNumber: event.target.value })
+      .subscribe((data) => {
+        this.validationResult = Object.assign({}, data);
+      });
   }
 
   ngOnInit(): void {}
