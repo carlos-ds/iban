f4fa775254328a95d24a8ff015572d26bd0f04d6
Karel De Smet <karel.de.smet@outlook.com>
2021-01-01 14:59:58 +0100

    Enforce https

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const app = express();
 
+app.use(requireHTTPS);
 app.use(express.static(__dirname + "/dist/iban"));
 
 app.get("/*", function (req, res) {
@@ -10,3 +11,16 @@ app.get("/*", function (req, res) {
 });
 
 app.listen(process.env.PORT || 8080, () => console.log("App started!"));
+
+// https://stackoverflow.com/questions/8605720/how-to-force-ssl-https-in-express-js
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (
+    !req.secure &&
+    req.get("x-forwarded-proto") !== "https" &&
+    process.env.NODE_ENV !== "development"
+  ) {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
