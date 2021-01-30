433cf2600163eca21935a42fc94e51bd5cac994f
Karel De Smet <karel.de.smet@outlook.com>
2021-01-30 09:16:56 +0100

    Remove obsolete import statement

--- src/app/services/iban.service.ts
+++ src/app/services/iban.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
 import { Iban, ValidationResult } from '../models/iban.model';
 import { environment } from '../../environments/environment';
 
