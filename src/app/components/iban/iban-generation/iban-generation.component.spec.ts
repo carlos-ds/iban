6cb383214dff1fe4f64821ab1157654c80a1dd9d
Karel De Smet <karel.de.smet@outlook.com>
2021-03-09 22:06:53 +0100

    Finished second test scenario for IbanGenerationComponent

--- src/app/components/iban/iban-generation/iban-generation.component.spec.ts
+++ src/app/components/iban/iban-generation/iban-generation.component.spec.ts
@@ -6,33 +6,33 @@ import { of } from 'rxjs';
 
 const mockIbans: Iban[] = [
   {
-    id: 1,
+    id: 5,
     accountNumber: 'BE76 5599 4230 6595',
-    createdAt: '2020-03-03T00:00:00.000Z',
+    createdAt: '2020-03-08T00:00:00.000Z',
     createdBy: 'GENERATE',
   },
   {
-    id: 2,
+    id: 4,
     accountNumber: 'BE67 4652 4188 4787',
-    createdAt: '2020-03-03T00:00:00.000Z',
+    createdAt: '2020-03-07T00:00:00.000Z',
     createdBy: 'GENERATE',
   },
   {
     id: 3,
     accountNumber: 'BE33 4978 4106 8346',
-    createdAt: '2020-03-03T00:00:00.000Z',
+    createdAt: '2020-03-06T00:00:00.000Z',
     createdBy: 'GENERATE',
   },
   {
-    id: 4,
+    id: 2,
     accountNumber: 'BE34 8984 2176 9090',
-    createdAt: '2020-03-03T00:00:00.000Z',
+    createdAt: '2020-03-05T00:00:00.000Z',
     createdBy: 'GENERATE',
   },
   {
-    id: 5,
+    id: 1,
     accountNumber: 'BE62 0729 0838 7761',
-    createdAt: '2020-03-03T00:00:00.000Z',
+    createdAt: '2020-03-04T00:00:00.000Z',
     createdBy: 'GENERATE',
   },
 ];
@@ -54,7 +54,16 @@ describe('IbanGenerationComponent', () => {
           provide: IbanService,
           useValue: {
             getGeneratedIbans: () => of(mockIbans),
-            createIban: () => of(['BE62 6501 2926 5661']),
+            createIban: () =>
+              of([
+                {
+                  id: 6,
+                  accountNumber: 'BE62 6501 2926 5661',
+                  createdAt: '2020-03-09T00:00:00.000Z',
+                  createdBy: 'GENERATE',
+                },
+                ...mockIbans.slice(-4),
+              ]),
             validateIban: (
               iban,
               has16Characters,
@@ -80,11 +89,11 @@ describe('IbanGenerationComponent', () => {
       },
     });
 
-    return { component };
+    return component;
   }
 
   it('should show the last 5 IBANs', async () => {
-    const component = await setup(mockIbans, 'BE76 5599 4230 6595');
+    const component = await setup(mockIbans, null);
     expect(screen.getByText('BE76 5599 4230 6595')).toBeTruthy();
     expect(screen.getByText('BE67 4652 4188 4787')).toBeTruthy();
     expect(screen.getByText('BE33 4978 4106 8346')).toBeTruthy();
@@ -95,13 +104,14 @@ describe('IbanGenerationComponent', () => {
     }).toThrow();
   });
 
-  it('should show the generated IBAN after clicking', async () => {
-    const component = await setup(mockIbans, 'BE76 5599 4230 6595');
+  it('should show the generated IBAN twice after clicking', async () => {
+    const component = await setup(mockIbans, null);
     const generateButton = screen.getByText(/Generate a random Belgian IBAN/i);
     expect(() => {
       screen.getByText('BE62 6501 2926 5661');
     }).toThrow();
     fireEvent.click(generateButton);
-    await screen.findByText('BE62 6501 2926 5661');
+    const generatedIban = await screen.findAllByText('BE62 6501 2926 5661');
+    expect(generatedIban.length).toEqual(2);
   });
 });
