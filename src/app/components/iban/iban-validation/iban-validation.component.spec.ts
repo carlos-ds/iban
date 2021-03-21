1bb6eb26d7d4524c9a25d5caaf320345858015df
Karel De Smet <karel.de.smet@outlook.com>
2021-03-21 11:56:54 +0100

    Updated test for IbanValidationComponent

--- src/app/components/iban/iban-validation/iban-validation.component.spec.ts
+++ src/app/components/iban/iban-validation/iban-validation.component.spec.ts
@@ -1,25 +1,66 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ValidationResult } from '../../../models/iban.model';
 import { IbanValidationComponent } from './iban-validation.component';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
+import { IbanService } from 'src/app/services/iban.service';
+import { of } from 'rxjs';
+
+const mockValidValidationResult: ValidationResult = {
+  iban: 'BE62 6501 2926 5661',
+  has16Characters: true,
+  startsWithBelgianPrefix: true,
+  endsWithNumbers: true,
+  hasValidBbanChecksum: true,
+  hasValidIbanChecksum: true,
+};
+
+const mockInvalidValidationResult: ValidationResult = {
+  iban: 'BE30 0000 0000 1212',
+  has16Characters: true,
+  startsWithBelgianPrefix: true,
+  endsWithNumbers: true,
+  hasValidBbanChecksum: false,
+  hasValidIbanChecksum: false,
+};
 
 describe('IbanValidationComponent', () => {
-  let component: IbanValidationComponent;
-  let fixture: ComponentFixture<IbanValidationComponent>;
+  async function setup(validationResult: ValidationResult) {
+    const component = await render(IbanValidationComponent, {
+      componentProviders: [
+        {
+          provide: IbanService,
+          useValue: {
+            validateIban: () => of(validationResult),
+          },
+        },
+      ],
+    });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ IbanValidationComponent ]
-    })
-    .compileComponents();
+    return component;
+  }
+
+  it('should show the input', async () => {
+    await setup(mockValidValidationResult);
+    expect(screen.getByLabelText('Validate a Belgian IBAN')).toBeTruthy();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(IbanValidationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should have data-valid set to true if validation result is valid', async () => {
+    await setup(mockValidValidationResult);
+    await userEvent.type(
+      screen.getByLabelText('Validate a Belgian IBAN'),
+      mockValidValidationResult.iban
+    );
+    const input = screen.getByLabelText('Validate a Belgian IBAN');
+    expect(input.getAttribute('data-valid')).toEqual('true');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have data-valid set to true if validation result is valid', async () => {
+    await setup(mockInvalidValidationResult);
+    await userEvent.type(
+      screen.getByLabelText('Validate a Belgian IBAN'),
+      mockInvalidValidationResult.iban
+    );
+    const input = screen.getByLabelText('Validate a Belgian IBAN');
+    expect(input.getAttribute('data-valid')).toEqual('false');
   });
 });
